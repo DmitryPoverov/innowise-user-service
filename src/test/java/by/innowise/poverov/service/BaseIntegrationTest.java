de6a488d1a7f517db62412a045c5cf4ab9771735
DP <did.ink@mail.ru>
2025-08-26 16:48:43 +0300

    BaseIntegrationTest.java - [Added] - Added an abstract class with base configuration for database connection.

---
+++ src/test/java/by/innowise/poverov/service/BaseIntegrationTest.java
@@ -0,0 +1,21 @@
+package by.innowise.poverov.service;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+abstract class BaseIntegrationTest {
+
+    @Container
+    private static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.32");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    }
+}
